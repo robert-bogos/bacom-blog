9588908ebd8b261ef256cb38f3dc751bb9dee3c0
Megan Thomas <methomas@adobe.com>
2023-02-17 13:36:17 -0800

    Improve test linting (#5)

--- test/sample.test.js
+++ test/sample.test.js
@@ -1,12 +1,10 @@
-/* eslint-disable no-undef */
-
 import { expect } from '@esm-bundle/chai';
 
 const testScript = 'this is a test';
 
 describe('Sample test for Husky', () => {
   it('passes', () => {
-    console.log('To be deleted after new tests are added'); // eslint-disable-line no-console
+    console.log('To be deleted after new tests are added');
     expect(testScript).to.equal(testScript);
   });
 });
